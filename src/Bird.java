775790b3ac42441e0442eb7e47303ec529ba01c2
halfa <m@richer.tk>
2014-07-28 21:46:55 +0200

    Refine bird comportement

--- src/Bird.java
+++ src/Bird.java
@@ -9,19 +9,20 @@ public class Bird extends InGameObj{
 	
 	public Bird() {
 		super(12,17);
-		x = 144/2; //l'oiseau est (et reste) au milieu de l'écran
+		//les coordonnées sont en haut à gauche
+		x = 144/3-6;
 		sinceLastBump = 0;
-		y = 256/2;
+		y = 256/2; //altitude
 	}
 	
 	//initialise le compteur de monté suite à un clic souris
 	public void bump() {
-		
+		y += 25;
 	}
 	
 	//Chute libre de l'oiseau -- action par défaut
 	public void fall(){
 		sinceLastBump++;
-		y += sinceLastBump;
+		y += sinceLastBump; //chute linéaire
 	}
 }
